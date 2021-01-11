9cbf41af0e9672188b4aff50e828c9b1430aefc3
tkgstrator <nasawake.am@gmail.com>
2021-01-12 08:41:45 +0900

    Sort water_level and event_type

--- pages/index.vue
+++ pages/index.vue
@@ -21,9 +21,10 @@
 <script>
 const BASE_STAGE_URL = "https://app.splatoon2.nintendo.net/images/coop_stage/"
 const STAGE_URL = { shakeup: "65c68c6f0641cc5654434b78a6f10b0ad32ccdee.png", shakeship: "e07d73b7d9f0c64e552b34a2e6c29b8564c63388.png", shakehouse: "6d68f5baa75f3a94e5e9bfb89b82e7377e3ecd2c.png", shakelift: "e9f7c7b35e6d46778cd3cbc0d89bd7e1bc3be493.png", shakeride: "50064ec6e97aac91e70df5fc2cfecf61ad8615fd.png" }
-const WATER_WORD = { 0: { ja: "", en: "Low tide" }, 1: { ja: "", en: "Normal" }, 2: { ja: "", en: "High tide" } }
-const EVENT_WORD = { 0: { ja: "", en: "No Event" }, 1: { ja: "", en: "Rush" }, 2: { ja: "", en: "Goldie Seeking" }, 3: { ja: "", en: "Griller" }, 4: { ja: "", en: "The Mothership" }, 5: { ja: "", en: "Fog" }, 6: { ja: "", en: "Cohock Charge" } }
-const STAGE_WORD = { shakeup: { ja: "", en: "Spawning Grounds" }, shakeship: { ja: "", en: "Marooner's Bay" }, shakehouse: { ja: "", en: "Lost Outpost" }, shakelift: { ja: "", en: "Salmonid Smokeyard" }, shakeride: { ja: "", en: "Ruins of Ark Polaris" } }
+const WATER_WORD = { 0: { ja: "干潮", en: "LT" }, 1: { ja: "通常", en: "NT" }, 2: { ja: "満潮", en: "HT" } }
+const EVENT_WORD = { 0: { ja: "イベントなし", en: "No Event" }, 1: { ja: "ラッシュ", en: "Rush" }, 2: { ja: "キンシャケ探し", en: "Goldie Seeking" }, 3: { ja: "グリル発進", en: "Griller" }, 4: { ja: "ハコビヤ襲来", en: "Mothership" }, 5: { ja: "霧", en: "Fog" }, 6: { ja: "ドスコイ大量発生", en: "Cohock Charge" } }
+const TITLE_WORD = { 0: { ja: "", en: "Total Golden Eggs"}, 1: { ja: "", en: "Total Golden Eggs(No Night)"}}
+const STAGE_WORD = { shakeup: { ja: "シェケナダム", en: "Spawning Grounds" }, shakeship: { ja: "難破船ドン・ブラコ", en: "Marooner's Bay" }, shakehouse: { ja: "海上集落シャケト場", en: "Lost Outpost" }, shakelift: { ja: "トキシラズいぶし工房", en: "Salmonid Smokeyard" }, shakeride: { ja: "朽ちた方舟 ポラリス", en: "Ruins of Ark Polaris" } }
 const BASE_WEAPON_URL = "https://app.splatoon2.nintendo.net/images/weapon/"
 const BASE_COOP_WEAPON_URL = "https://app.splatoon2.nintendo.net/images/coop_weapons/"
 const WEAPONS = {
@@ -124,22 +125,29 @@ export default {
       var tr = document.createElement("tr")
 
       // イベント名追加
-      var td = document.createElement("td")
+      var td = document.createElement("th")
       if (data[0][i]["water_level"] != 3) {
-        let water = document.createElement("p")
-        let event = document.createElement("p")
-        water.className = "water_level"
-        event.className = "event_type"
+        let title = document.createElement("p")
+        title.className = "event_type"
 
-        water.textContent = WATER_WORD[data[0][i]["water_level"]]["en"]
-        event.textContent = EVENT_WORD[data[0][i]["event_type"]]["en"]
-        td.appendChild(water)
-        td.appendChild(event)
+        if (data[0][i]["event_type"] == 6)
+          title.textContent = EVENT_WORD[data[0][i]["event_type"]]["en"]
+        else
+          title.textContent = WATER_WORD[data[0][i]["water_level"]]["en"] + " " + EVENT_WORD[data[0][i]["event_type"]]["en"]
+        td.appendChild(title)
+        // td.appendChild(event)
       }
       tr.appendChild(td)
 
+      if (data[0][i]["water_level"] == 3) {
+        let title = document.createElement("p")
+        title.className = "title"
+        title.textContent = TITLE_WORD[data[0][i]["event_type"]]["en"]
+        td.appendChild(title)
+      }
+
       // 各種レコードを載せる
-      for (let id = 0; id <= 4; id++) {
+      for (let id = 0; id <= 4; id++) { // idはステージID
         const weapon_list = data[id][i]["weapon_list"].split(",")
         const golden_ikura_num = data[id][i]["golden_ikura_num"]
         const seed = data[id][i]["seed"] // ゲームシード情報（ないかもしれない）
@@ -193,299 +201,7 @@ export default {
 </script>
 
 <style lang="scss">
-@font-face {
-  font-family: "SplatFont";
-  src: url("~@/assets/fonts/Splatfont.woff") format("woff"),
-    url("~@/assets/fonts/Splatfont.woff2") format("woff2");
-}
-@font-face {
-  font-family: "SplatFont2";
-  src: url("~@/assets/fonts/Splatfont2.woff") format("woff"),
-    url("~@/assets/fonts/Splatfont2.woff2") format("woff2");
-}
-
-div {
-  font-family: "Splatfont";
-  display: block;
-
-  .page {
-    min-height: 100%;
-    box-sizing: border-box;
-  }
-
-  .coop-overfishing {
-    position: relative;
-    background-image: url(https://app.splatoon2.nintendo.net/images/bundled/8c15ceb605300fbc22963fabcb09fb22.png);
-    background-size: 255px 127px;
-    background-repeat: repeat;
-    overflow: hidden;
-    width: 100%;
-
-    .navigation-bar {
-      display: flex;
-      flex-wrap: nowrap;
-      position: relative;
-      background-image: url(https://app.splatoon2.nintendo.net/images/bundled/26c8c0b675f643f384991434574baf71.png);
-      background-size: 80px 80px;
-      color: #fff;
-      line-height: 48px;
-      min-height: 48px;
-      font-family: "Splatfont";
-      z-index: 1000;
-
-      & h1 {
-        text-align: center;
-        font-size: 18px;
-        padding: 0 16px;
-        flex-grow: 1;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-      }
-    }
-  }
-
-  .coop-overfishing-header {
-    position: relative;
-    text-align: center;
-    padding: 16px 0 30px;
-
-    h3 {
-      font-family: "Splatfont";
-      min-width: 200px;
-      background-position-x: 50%;
-      background-position-y: center;
-      box-sizing: border-box;
-      color: #ff5033;
-
-      span {
-        padding-top: 12px;
-        min-width: 200px;
-        background-position: 50%;
-        box-sizing: border-box;
-        display: inline-block;
-        background-image: url("https://app.splatoon2.nintendo.net/images/bundled/cc7454b6eb6b8e16852ed21f00e0fd40.png");
-        background-size: 488px 52px;
-        height: 52px;
-        vertical-align: middle;
-      }
-
-      &::before {
-        background-position: 0px center;
-        width: 42px;
-        content: "";
-        display: inline-block;
-        background-image: url("https://app.splatoon2.nintendo.net/images/bundled/cc7454b6eb6b8e16852ed21f00e0fd40.png");
-        background-size: 488px 52px;
-        height: 52px;
-        vertical-align: middle;
-      }
-
-      &::after {
-        background-position: 100% center;
-        width: 42px;
-        content: "";
-        display: inline-block;
-        background-size: 488px 52px;
-        background-image: url("https://app.splatoon2.nintendo.net/images/bundled/cc7454b6eb6b8e16852ed21f00e0fd40.png");
-        height: 52px;
-        vertical-align: middle;
-      }
-    }
-
-    h2 {
-      position: relative;
-      background-image: url("https://app.splatoon2.nintendo.net/images/bundled/a185b309f5cdad94942849070de04ce2.png");
-      background-size: contain;
-      width: 332px;
-      margin: 0 auto;
-      height: 112.5px;
-      z-index: 1;
-
-      & span {
-        display: none;
-      }
-    }
-
-    h3 {
-      min-width: 200px;
-      background-position-x: 50%;
-      background-position-y: center;
-      box-sizing: border-box;
-      color: #ff5033;
-    }
-  }
-
-  .coop-overfishing-list-wrapper {
-    position: relative;
-    background: #150909;
-    padding-top: 16px;
-    padding-bottom: 90px;
-    padding-bottom: calc(constant(safe-area-inset-bottom) + 90px);
-    padding-bottom: calc(env(safe-area-inset-bottom) + 90px);
-    z-index: 1;
-
-    &::before {
-      // background: url(https://app.splatoon2.nintendo.net/images/bundled/b24ee02521f18ebe1bf8b05e1396c3dc.png);
-      background: #150909;
-      content: "";
-      position: absolute;
-      width: 100%;
-      height: 20px;
-      margin-top: -36px;
-      left: 0;
-      -webkit-mask-image: url("~static/3ff65812f7fc0aff176df4c78172691c.png");
-      // mask-image: url(https://app.splatoon2.nintendo.net/images/bundled/3ff65812f7fc0aff176df4c78172691c.png);
-      -webkit-mask-repeat: no-repeat;
-      mask-repeat: no-repeat;
-      -webkit-mask-size: 100% 100%;
-      mask-size: 100% 100%;
-    }
-  }
-
-  #coop-overfishing-records {
-    table {
-      font-family: "Splatfont2";
-      // table-layout: auto;
-      table-layout: fixed;
-      width: 100%;
-      max-width: 1200px;
-      // text-align: center;
-      background: #150909;
-      // border-collapse: collapse;
-
-      background-size: 40px 40px;
-      padding: 0 4px;
-      margin: 0 auto;
-      margin-bottom: 24px;
-      color: #fff;
-    }
-
-    thead {
-      th {
-        img {
-          object-fit: scale-down;
-          width: 100%;
-          max-width: 12em;
-          border-radius: 0.5em;
-        }
-      }
-
-      p {
-        font-size: 1.2em;
-      }
-    }
-
-    td {
-      display: float;
-      background: rgba(255, 255, 255, 0.1);
-      background-image: url(https://app.splatoon2.nintendo.net/images/bundled/b24ee02521f18ebe1bf8b05e1396c3dc.png);
-      background-size: 40px 40px;
-      border-radius: 0.5em;
-      padding: 0.3em;
-      border: 0.1em solid #150604;
-      // height: 60px;
-      vertical-align: middle;
-      text-align: center;
-      // width: 100px;
-
-      &::before {
-        white-space: pre;
-      }
-    }
-
-    .eggs {
-      font-size: 125%;
-      color: yellow;
-      text-shadow: 0.15em 0.15em 0 #000;
-
-      &::before {
-        display: inline-block;
-        content: "";
-        background: url(https://app.splatoon2.nintendo.net/images/bundled/3aa6fb4ec1534196ede450667c1183dc.png)
-          50% no-repeat;
-        background-repeat: no-repeat;
-        background-position: 50%;
-        background-size: contain;
-        width: 1.2em;
-        height: 1em;
-        vertical-align: middle;
-        margin-top: -0.1em;
-        filter: drop-shadow(0.15em 0.15em 0 #000);
-      }
-    }
-
-    .norecords {
-      font-size: 125%;
-      color: yellow;
-      text-shadow: 0.15em 0.15em 0 #000;
-    }
-
-    .golden_ikura_num {
-      &::before {
-        content: "X";
-        font-size: 0.6em;
-        margin: 0 0.1em;
-      }
-    }
-
-    ul {
-      margin: 0;
-      padding: 0;
-    }
-
-    .weapons {
-      display: flex;
-      width: 100%;
-      list-style-type: none;
-      justify-content: center;
-      cursor: pointer;
-
-      li {
-        background: #000;
-        width: 2em;
-        height: 2em;
-        border-radius: 1em;
-      }
-
-      li:not(:last-child) {
-        margin-right: 0.2em;
-      }
-
-      img {
-        width: 2em;
-      }
-    }
-
-    @media (max-width: 1020px) {
-      thead {
-        p {
-          display: none;
-        }
-      }
-      .weapons {
-        display: none;
-      }
-    }
-
-    .links {
-      img {
-        width: 1.8em;
-      }
-    }
-  }
-
-  .coop-overfishing-title {
-    text-align: center;
-  }
-
-  a {
-    text-decoration: none;
-    color: #ddd;
-  }
-
-  .oceancalc-button {
-    cursor: pointer;
-  }
-}
+@import "~assets/sass/style.scss";
+@import "~assets/sass/splatnet2.scss";
+@import "~assets/sass/salmonrecords.scss";
 </style>
